35ad9a907cf420c839862f0d0e213a7253ec92f3
özlem arslan <ozlemarslan1101@gmail.com>
2025-01-12 19:47:51 +0300

    düzenlemeler yapıldı

--- app.js
+++ app.js
@@ -1,53 +1,81 @@
 import { modlar } from "./veri.js";
 
-
 function UIGöster() {
-// modlar dizisindeki her bir mod için aşağıdaki işlemler yapılacak
-const kokEleman = document.querySelector("#root");
-const baslikElemani = document.createElement("h1");
-const modListesiElemani = document.createElement("div");
-modListesiElemani.id = "mod-listesi";
+  // state değişkenleri oluşturuluyor
+  let premiumKullanici = false;
+  let calmaListesi = [];
 
-baslikElemani.textContent = "ModApp v.1.0.0";
-kokEleman.append(baslikElemani);
-kokEleman.append(modListesiElemani);
+  // modlar dizisindeki her bir mod için aşağıdaki işlemler yapılacak
+  const kokEleman = document.querySelector("#root");
+  const baslikElemani = document.createElement("h1");
+  const modListesiElemani = document.createElement("div");
+  modListesiElemani.id = "mod-listesi";
 
-modlar.forEach((mod) => {
-  // mod resmi ve ismi için div oluşturuldu
+  baslikElemani.textContent = "ModApp v.1.0.0";
+  kokEleman.append(baslikElemani);
+  kokEleman.append(modListesiElemani);
 
-  const modElemani = document.createElement("div");
-  modElemani.className = "mod";
+  const bodyElemani = document.querySelector("body");
+  const calmaListesiElemani = document.createElement("div");
+  calmaListesiElemani.id = "calma-listesi";
+  bodyElemani.append(calmaListesiElemani);
 
-  // mod ismi için h2 oluşturuldu
+  modlar.forEach((mod) => {
+    // mod resmi ve ismi için div oluşturuldu
 
-  const modAdiElemani = document.createElement("h2");
-  modListesiElemani.append(modElemani);
-  modElemani.append(modAdiElemani);
-  modAdiElemani.textContent = mod.modAdi;
+    const modElemani = document.createElement("div");
+    modElemani.className = "mod";
 
-  // mod resmi için img oluşturuldu
+    // mod ismi için h2 oluşturuldu
 
-  const modGorselElemani = document.createElement("img");
-  modGorselElemani.src = `./img/${mod.modAdi}.jpg`;
-  modElemani.append(modGorselElemani);
+    const modAdiElemani = document.createElement("h2");
+    modListesiElemani.append(modElemani);
+    modElemani.append(modAdiElemani);
+    modAdiElemani.textContent = mod.modAdi;
 
-  // ses dosyası için audio oluşturuldu
-  const sesElemani = document.createElement("audio");
-  sesElemani.src = `./audio/${mod.modAdi}-sound.mp3`;
-  sesElemani.loop = true; // sesin sürekli çalması sağlandı
+    // mod resmi için img oluşturuldu
 
-  // mod resmine tıklanınca müziği başlatması sağlandı
-  modElemani.addEventListener("click", () => {
-    if (sesElemani.paused) {
-      sesElemani.play();
-      modElemani.classList.add("aktif-mod"); // Mod div elemanına .aktif-mod css classı ekleniyor.
-    } else {
-      sesElemani.pause();
-      modElemani.classList.remove("aktif-mod"); // Mod div elemanından .aktif-mod css classı kaldırılıyor.
-    }
+    const modGorselElemani = document.createElement("img");
+    modGorselElemani.src = `./img/${mod.modAdi}.jpg`;
+    modElemani.append(modGorselElemani);
+
+    // ses dosyası için audio oluşturuldu
+    const sesElemani = document.createElement("audio");
+    sesElemani.src = `./audio/${mod.modAdi}-sound.mp3`;
+    sesElemani.loop = true; // sesin sürekli çalması sağlandı
+
+    // mod resmine tıklanınca müziği başlatması sağlandı
+    modElemani.addEventListener("click", () => {
+      if (sesElemani.paused) {
+        sesElemani.play();
+        modElemani.classList.add("aktif-mod"); // Mod div elemanına .aktif-mod css classı ekleniyor.
+        calmaListesi.push(mod.modAdi); // Çalma listesine modun adı ekleniyor.
+        
+      } else {
+        sesElemani.pause();
+        modElemani.classList.remove("aktif-mod"); // Mod div elemanından .aktif-mod css classı kaldırılıyor.
+        calmaListesi = calmaListesi.filter((calinanMod) => calinanMod !== mod.modAdi); // Çalma listesinden modun adı çıkarılıyor.
+      }
+      calmaListesiGoster(calmaListesi); 
+    //   console.log(calmaListesi); 
+    });
   });
-});
-
 }
 
 UIGöster();
+
+
+    function calmaListesiGoster(calmaListesi) {
+        const calmaListesiElemani = document.querySelector("#calma-listesi");
+        calmaListesiElemani.innerHTML = ""; // Çalma listesi elemanının içeriği temizleniyor.
+        calmaListesi.forEach((calinanMod) => {
+            const calinanModElemani = document.createElement("p"); // Çalma listesi elemanı için p elemanı oluşturuluyor.
+            calinanModElemani.textContent = calinanMod; // Çalınan modun adı p elemanına ekleniyor.
+            calmaListesiElemani.append(calinanModElemani); // Çalma listesi elemanına p elemanı ekleniyor.
+        });
+    
+        const toplamModElemani = document.createElement("p"); 
+        toplamModElemani.textContent = calmaListesi.length; 
+        calmaListesiElemani.append(toplamModElemani);
+    }
+
